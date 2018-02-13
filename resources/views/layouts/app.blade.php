235396acf8f1746d14f2ce6ed23e7c9db2f6eee1
Dimas Wibowo <daemswibowo@gmail.com>
2018-02-13 16:41:03 +0800

    tambah custom css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     @yield('styles')
 </head>
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+    {!! Html::style('css/custom.css') !!}
     <!-- Styles -->
     <style>
     html, body {
