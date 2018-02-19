aa64aca8d55c04a8b40c94b58865cfec7d03137f
Dimas Wibowo <daemswibowo@gmail.com>
2018-02-19 21:34:12 +0800

    reksi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,9 +88,9 @@
 
     <div class="content">
         <div class="title">
-            SIM SURAT
+            <img src="{{ asset('img/logo.png') }}" height="140px" alt="">
         </div>
-        <p>Sistem Manajemen Surat</p>
+        <p><b>SIMSURAT DINAS PARIWISATA KOTA GORONTALO</b></p>
 
         <div class="links">
             <a href="{{ route('surat.index') }}">Daftar Surat Masuk/Keluar</a>
